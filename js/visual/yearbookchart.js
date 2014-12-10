ad5e3d8bd6a94a84dd7a833eee5c422aa28bd28f
HuangShuhong <a215974929@qq.com>
2014-12-10 14:31:48 +0800

    图表生成修改，fashinochart 取消

--- js/visual/yearbookchart.js
+++ js/visual/yearbookchart.js
@@ -4,27 +4,91 @@
 	yearbook.lineChartXml = "<chart unescapeLinks='0'anchorBgColor='ed821a'caption=''subCaption='学术发展趋势(期刊文章)1915-2013'showPlotBorder='1'plotBorderThickness='2'plotBorderColor='ed790c'plotBorderAlpha='80'plotFillColor='fdf8e5'plotFillAlpha='50'canvasBorderColor='645e60'canvasBorderThickness='0'anchorAlpha='100'anchorBorderColor='ed821a'anchorRadius='2.5'bgcolor='ffffff'showborder='100'centerYaxisName='1'xAxisName='(单位：个)'numDivLines='11'yAxisValuesStep='3'labelStep='13'alternateHGridColor='ffffff'showValues='0'formatNumberScale='0'formatNumber='0'baseFontSize='14'><set link=''value='1'label='1915年'hoverText='1915年 : 1'></set><set link=''value='1'label='1917年'hoverText='1917年 : 1'></set><set link=''value='1'label='1925年'hoverText='1925年 : 1'></set><set link=''value='1'label='1929年'hoverText='1929年 : 1'></set><set link=''value='1'label='1946年'hoverText='1946年 : 1'></set><set link=''value='1'label='1950年'hoverText='1950年 : 1'></set><set link=''value='2'label='1951年'hoverText='1951年 : 2'></set><set link=''value='2'label='1952年'hoverText='1952年 : 2'></set><set link=''value='8'label='1953年'hoverText='1953年 : 8'></set><set link=''value='13'label='1954年'hoverText='1954年 : 13'></set><set link=''value='14'label='1955年'hoverText='1955年 : 14'></set><set link=''value='21'label='1956年'hoverText='1956年 : 21'></set><set link=''value='26'label='1957年'hoverText='1957年 : 26'></set><set link=''value='19'label='1958年'hoverText='1958年 : 19'></set><set link=''value='30'label='1959年'hoverText='1959年 : 30'></set><set link=''value='32'label='1960年'hoverText='1960年 : 32'></set><set link=''value='17'label='1961年'hoverText='1961年 : 17'></set><set link=''value='16'label='1962年'hoverText='1962年 : 16'></set><set link=''value='24'label='1963年'hoverText='1963年 : 24'></set><set link=''value='20'label='1964年'hoverText='1964年 : 20'></set><set link=''value='20'label='1965年'hoverText='1965年 : 20'></set><set link=''value='6'label='1966年'hoverText='1966年 : 6'></set><set link=''value='2'label='1970年'hoverText='1970年 : 2'></set><set link=''value='5'label='1972年'hoverText='1972年 : 5'></set><set link=''value='13'label='1973年'hoverText='1973年 : 13'></set><set link=''value='13'label='1974年'hoverText='1974年 : 13'></set><set link=''value='18'label='1975年'hoverText='1975年 : 18'></set><set link=''value='13'label='1976年'hoverText='1976年 : 13'></set><set link=''value='19'label='1977年'hoverText='1977年 : 19'></set><set link=''value='40'label='1978年'hoverText='1978年 : 40'></set><set link=''value='78'label='1979年'hoverText='1979年 : 78'></set><set link=''value='124'label='1980年'hoverText='1980年 : 124'></set><set link=''value='168'label='1981年'hoverText='1981年 : 168'></set><set link=''value='218'label='1982年'hoverText='1982年 : 218'></set><set link=''value='226'label='1983年'hoverText='1983年 : 226'></set><set link=''value='268'label='1984年'hoverText='1984年 : 268'></set><set link=''value='302'label='1985年'hoverText='1985年 : 302'></set><set link=''value='320'label='1986年'hoverText='1986年 : 320'></set><set link=''value='355'label='1987年'hoverText='1987年 : 355'></set><set link=''value='339'label='1988年'hoverText='1988年 : 339'></set><set link=''value='343'label='1989年'hoverText='1989年 : 343'></set><set link=''value='353'label='1990年'hoverText='1990年 : 353'></set><set link=''value='413'label='1991年'hoverText='1991年 : 413'></set><set link=''value='428'label='1992年'hoverText='1992年 : 428'></set><set link=''value='370'label='1993年'hoverText='1993年 : 370'></set><set link=''value='718'label='1994年'hoverText='1994年 : 718'></set><set link=''value='754'label='1995年'hoverText='1995年 : 754'></set><set link=''value='783'label='1996年'hoverText='1996年 : 783'></set><set link=''value='857'label='1997年'hoverText='1997年 : 857'></set><set link=''value='960'label='1998年'hoverText='1998年 : 960'></set><set link=''value='1147'label='1999年'hoverText='1999年 : 1147'></set><set link=''value='1552'label='2000年'hoverText='2000年 : 1552'></set><set link=''value='1911'label='2001年'hoverText='2001年 : 1911'></set><set link=''value='2556'label='2002年'hoverText='2002年 : 2556'></set><set link=''value='3510'label='2003年'hoverText='2003年 : 3510'></set><set link=''value='3853'label='2004年'hoverText='2004年 : 3853'></set><set link=''value='3620'label='2005年'hoverText='2005年 : 3620'></set><set link=''value='4247'label='2006年'hoverText='2006年 : 4247'></set><set link=''value='5282'label='2007年'hoverText='2007年 : 5282'></set><set link=''value='5781'label='2008年'hoverText='2008年 : 5781'></set><set link=''value='6423'label='2009年'hoverText='2009年 : 6423'></set><set link=''value='6742'label='2010年'hoverText='2010年 : 6742'></set><set link=''value='7530'label='2011年'hoverText='2011年 : 7530'></set><set link=''value='4680'label='2012年'hoverText='2012年 : 4680'></set><set link=''value='2013'label='2013年'hoverText='2013年 : 2013'></set><set></set><set></set></chart>";
 
 	var chartData={"chart": {
-      "caption": "Monthly revenue for last year",
-      //"subCaption": "Harry's SuperMart",
-      "xAxisName": "Month",
-      "yAxisName": "Revenues",
-      "numberPrefix": "",
-      "theme": "fint"
-   },"data":[]};
+      "caption": "Split of Sales by Product Category",
+      "subCaption": "In top 5 stores last month",
+      "yAxisname": "Sales (In USD)",
+      "numberPrefix": "$",
+      "paletteColors": "#0075c2,#1aaf5d",
+      "bgColor": "#ffffff",
+      "showBorder": "0",
+      "showHoverEffect": "1",
+      "showCanvasBorder": "0",
+      "usePlotGradientColor": "1",
+      "plotBorderAlpha": "10",
+      "legendBorderAlpha": "1",
+      "legendShadow": "0",
+      "placevaluesInside": "1",
+      "valueFontColor": "#ffffff",
+      "showXAxisLine": "0",
+      "xAxisLineColor": "#999999",
+      "divlineColor": "#999999",
+      "divLineDashed": "1",
+      "showAlternateVGridColor": "1",
+      "subcaptionFontBold": "0",
+      "subcaptionFontSize": "18"
+   },
+      "categories": [
+      {
+         "category": [
+            {
+               "label": "Food"
+            },
+            {
+               "label": "Apparels"
+            },
+            {
+               "label": "Electronics"
+            },
+            {
+               "label": "Household"
+            },
+            {
+               "label": "peach"
+            }
+         ]
+      }
+   ],
+   "dataset": [
+      {
+         "seriesname": "",
+		 "color" : '8BBA00',
+         "data": [
+            {
+               "value": "17000"
+            },
+            {
+               "value": "19500"
+            },
+            {
+               "value": "12500"
+            },
+            {
+               "value": "14500"
+            },
+            {
+               "value": "17500"
+            }
+         ]
+      }
+
+   ]
+
+   };
 
 
 	
 	$(document).ready(function () {
+			//生成图表
 			$(".btn-createbar").click(function(){
 				var optoin={
-					"type":yearbook.charType,
+					"type": yearbook.charType,
 					"container":$("#divRightColumn"),
 					"data": chartData
 					//"data" : yearbook.tw
 					//"data" : yearbook.twbar
 					//"data" : d4
 					}
-				createData(searchresult);
+				//createData(searchresult);
 				createChart(optoin);
 			
 			});
@@ -59,8 +123,8 @@ function createChart(option) {
 	var defOption={
 		"container":$("#divRightColumn"),
 		"type":"column",
-		"width": 600,
-		"height": 360,
+		"width": 610,
+		"height": 500,
 		"data": yearbook.columnData,
 		"dataFormat" : "json"
 	}
@@ -74,32 +138,19 @@ function createChart(option) {
 	var h=option["height"];
 	var str=option["data"];
 	var typePath="";
-	var swfPath="/js/visual/FusionCharts/";
+	//var swfPath="/js/visual/FusionCharts/";
+	var swfPath="js/visual/FusionCharts/";
 	var container=option["container"];
 	var formatTp=option.dataFormat||"json";
 	switch (option["type"]){
 		case "bar": 
-		typePath="Bar2D";
-		break;
-		case "column":
-		typePath="Column2D";
-		break;
-		case "pie":
-		typePath="Doughnut2D";
-		break;
-		case "line":
-		typePath="Line";
-		break;
-
-		//3d
-		case "bar": 
 		typePath="MSBar3D";
 		break;
-		case "3dcolumn":
+		case "column":
 		typePath="MSColumn3D";
 		break;
 		case "pie":
-		typePath="Pie3D";
+		typePath="Doughnut3D";
 		break;
 		case "line":
 		typePath="MSLine";
@@ -118,7 +169,7 @@ function createChart(option) {
 /*
 ** 生成条件
 **根据提供数据生成页面分类条件
-**@para {string}orginData 生成图的各类
+**@para {string}orginData 检索源数据
 **@para {string}container 生成图的容器
 */
 
@@ -129,28 +180,42 @@ function createContion(orginData){
 	}
 	var data=[];
 	var schtp=["area","indicator","class","date"];
+	var chartColor=["8BBA00","8BBA00","8BBA00","8BBA00"];
+	//图表种类
 	var type=$("#js_char_type").val();
 		schtp.splice(schtp.indexOf(type),1);
 	var str="",strobj="";
-	//横坐标
+	
+	//分类或固定项选取
 	for(var i=0,j=orginData[type];i<j.length;i++){
 		
-		if(i<4){
-			chartData["data"][i]={};
-			chartData["data"][i]["label"]=j[i].value;
-			strobj+="<p data-id="+(i<10?("0"+i):i)+">"+j[i].value+"</p>";//前几年备选
+		if(i<4){//默认选中
+			if(1){//坐标
+				chartData["categories"][i]={};
+				chartData["categories"][i]["label"]=j[i].value;//取值
+			} else {//分类
+				chartData["dataset"][i]={};
+				chartData["dataset"][i]["seriesname"]=j[i].value;//取值
+				chartData["dataset"][i]["color"]=chartColor[i];
+			}
+			
+			strobj+="<p data-id="+(i<10?("0"+i):i)+">"+j[i].value+"</p>";
 		}else{
-			str+="<p data-id="+(i<10?("0"+i):i)+">"+j[i].value+"</p>";//后几个不先
+			str+="<p data-id="+(i<10?("0"+i):i)+">"+j[i].value+"</p>";
 		}
 		
 	}
-	//chartData["data"]=data;
-	$(".js_chart_tar").html(strobj);
-	$(".js_chart_orgin").html(str);
-	//固定项
+	if(1){
+		$(".js_chart_tar").html(strobj);
+		$(".js_chart_orgin").html(str);
+
+	} else {
+		
+	}
+	//固定项,生成下拉列表
 	for(var k=0;k<schtp.length;k++){
 		str="";
-		for(var m=0,n=schtp[k];m<orginData[n].length;m++){
+		for(var m=0,n=schtp[k];m<orginData[n].length;m++){//数据与坐标对应
 			str+="<option value='"+(m<10?("0"+m):m)+"'>"+orginData[n][m].value+"</option>";
 			//chartData["data"][m]["value"]=n[m].value;
 		}
@@ -164,14 +229,18 @@ function createData(orginData){
 	var n1,n2,n3,n4,index=[] ,value,k=[],dataObj={},strValue=[];
 	var schtp=["area","indicator","class","date"];
 	var tempData=orginData["dataset"];
+	
 	var type=$("#js_char_type").val();
 	
 			
 	//获取标题
-	chartData.chart.caption=$("#js_charttt").val()||"我的图表";
+	chartData.chart.caption=$("#js_charttt").val()||"图表名称";
 	chartData.chart.xAxisName=$("#js_char_type").find("option:selected").text();
 	chartData.chart.yAxisName="index";
-	for(var h=0;h<3;h++){
+	chartData.chart.legendShadow=($(".hidechart:checked").size())>0?0:1;
+	chartData.chart.showValues=($(".datatag:checked").size())>0?0:1;
+	
+	for(var h=0;h<schtp.length;h++){
 		var tp=$("#js_opt"+h).attr("data-tp");
 		dataObj[tp]=[];
 		dataObj[tp][0]=$("#js_opt"+h).val();
@@ -226,6 +295,14 @@ $(document).ready(function () {
 		$(".mod-static").show();
 		//表单初始化
 		createContion(searchresult);
+	});
+	/* 返回 */
+	$(".icon-goback").click(function(){
+		
+		$(".mod-schbox").show();
+		$(".mod-static").hide();
+		//表单初始化
+		//createContion(searchresult);
 	});
 	
 		//选择图表类型
